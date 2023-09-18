5140eb29646721e3fcba493bd6e08f5866ac9d95
austgh <46312526+austgh@users.noreply.github.com>
2023-09-18 17:22:48 +0800

    Initial commit

---
+++ src/main/java/com/example/springboottomcat/TestController.java
@@ -0,0 +1,23 @@
+package com.example.springboottomcat;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author think
+ * @date 2023年09月13日 17:23
+ */
+@Slf4j
+@RestController
+public class TestController {
+
+    @GetMapping("/getTest")
+    public void getTest(int num) throws Exception {
+        log.info("{} 接受到请求:num={}", Thread.currentThread().getName(), num);
+        TimeUnit.HOURS.sleep(1);
+    }
+}
+
